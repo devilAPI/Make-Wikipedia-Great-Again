db23e939199e46db5b0b601ec7b865268b6f11d9
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-09-10 23:26:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const proxy-fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 const website = "wikipedia.org"
 const transferprotocol = "https"
 
 module.exports = async (req, res) => {
   res.setHeader('Cache-Control', 's-maxage=3, stale-while-revalidate');
-  const html = (await (await proxy-fetch(`$transferprotocol://$website${req.url}`)).text())
+  const html = (await (await fetch(`$transferprotocol://$website${req.url}`)).text())
     .replace(/(href=.)$transferprotocol?:\/\/$website/g, `$1//${req.headers.host}`)
     .replace(
       '</head>',
